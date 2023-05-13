6bf5ea7321b9b4d65354e9991ad84503a407ff45
Toby Inkster <mail@tobyinkster.co.uk>
2023-05-13 22:11:08 +0100

    bump version; changelog

--- lib/Story/Interact/WWW.pm
+++ lib/Story/Interact/WWW.pm
@@ -5,7 +5,7 @@ use warnings;
 package Story::Interact::WWW;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001005';
+our $VERSION   = '0.001006';
 
 use constant DISTRIBUTION => 'Story-Interact-WWW';
 
